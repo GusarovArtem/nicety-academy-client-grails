8c548818c23033ddbe4cd3bc672cdeb9996a2e74
GusarovArtem <artemmaksimovich1@gmail.com>
2023-03-03 09:08:05 +0200

    kafka: added course listener

---
+++ src/main/groovy/academy/microservices/AcademyCourseListener.groovy
@@ -0,0 +1,24 @@
+package academy.microservices
+
+import academy.course.AcademyCourse
+import io.micronaut.configuration.kafka.annotation.KafkaListener
+import io.micronaut.configuration.kafka.annotation.Topic
+
+@KafkaListener
+class AcademyCourseListener {
+
+    @Topic('createCourse')
+    void createCourse(AcademyCourse course) {
+        course.save flush: true
+    }
+
+    @Topic('updateCourse')
+    void updateCourse(AcademyCourse course) {
+        course.save flush: true
+    }
+
+    @Topic('deleteCourse')
+    void deleteCourse(Long id) {
+        AcademyCourse.findById(id).delete()
+    }
+}
