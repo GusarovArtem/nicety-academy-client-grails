082c781d6b125290259ec00ccab2314a5064c29f
GusarovArtem <artemmaksimovich1@gmail.com>
2023-05-20 05:35:21 +0300

    added persisting models

---
+++ grails-app/controllers/academy/user/client/ClientController.groovy
@@ -0,0 +1,16 @@
+package academy.user.client
+
+import academy.trait.ControllerTrait
+import grails.gorm.transactions.Transactional
+
+@Transactional(readOnly = true)
+class ClientController implements ControllerTrait {
+
+//    TODO
+//       message to ClientManager
+//       see courses
+//       see teachers
+
+
+}
+
